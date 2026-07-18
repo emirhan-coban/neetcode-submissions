8a75d0fe79faf71a93b8ebebe401c46b76c14f19
emirhan-coban <71819553+emirhan-coban@users.noreply.github.com>
2026-07-18 23:30:48 +0300

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean = []
+        for i in s:
+            if i.isalnum():
+                clean.append(i.lower())
+        cleanString = ''.join(clean)
+        reverseString = cleanString[::-1]
+        return cleanString == reverseString
